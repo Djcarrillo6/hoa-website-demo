07a92c8039a02c473e9f9899adbaf5cd457047df
Djcarrillo6 <Djcarrillo06@gmail.com>
2021-07-23 17:14:48 -0700

    Added media query for iPhone 11 devices.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0c41465828ae169275bd"; }
+/******/ 		__webpack_require__.h = function() { return "26dba17d964430870889"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
