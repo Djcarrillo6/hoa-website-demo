026cc5f6a47771e04953c0a52de2226cb2bc35bb
Djcarrillo6 <Djcarrillo06@gmail.com>
2021-08-05 11:29:50 -0700

    Added comment notes to Footer.js component.

--- components/Layouts/Footer.js
+++ components/Layouts/Footer.js
@@ -4,6 +4,23 @@ import Link from 'next/link';
 const Footer = () => {
 	const asystHref = 'https://myaccount.powerstonepm.com/_UserLogin?Mode=Register';
 
+	// TODO: env.local VARIABLES
+	const client_id = env.local.client_id || '';
+	const app_id = env.local.app_id || '';
+	const deploymentURL = env.local.deploymentURL || '';
+	const asystAppAuthEndpoint = `https://myaccount.powerstonepm.com/_AppAuth?redirect_uri=https://${deploymentURL}/requestTokens&client_id=${client_id}&app_id=${app_id}`;
+
+
+	/*
+	TODO: Create an onClick function to add parameters to the URL & navigate the user to the new URL.
+
+	* JS Add Parameter To URL Without Reloading Page.
+	<script>
+		var currentURL = window.location.protocol + "//" + window.location.host + window.location.pathname + '?arg=1';    
+		window.history.pushState({ path: currentURL }, '', currentURL);
+	</script>
+	*/
+
 	let currentYear = new Date().getFullYear();
 
 	return (
@@ -48,6 +65,13 @@ const Footer = () => {
 										Admin
 									</a>
 								</Link>
+								{/* 
+								<Link href={asystAppAuthEndpoint}>
+									<a className='nav-link' target='_blank'>
+										Admin
+									</a>
+								</Link>
+								 */}
 							</button>
 						</div>
 					</div>
